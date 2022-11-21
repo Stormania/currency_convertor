0e83a16cf07cf0534df0904185ca209a90a0636e
Ştefan <Stormania94@gmail.com>
2022-11-21 19:34:50 +0200

    modified:   analysis_options.yaml 	modified:   lib/main.dart 	modified:   pubspec.yaml 	modified:   test/widget_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,24 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Currency Convertor',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          fontFamily: 'Raleway',
-          primarySwatch: Colors.red,
-          hintColor: Colors.grey),
+        fontFamily: 'Raleway',
+        primarySwatch: Colors.red,
+        hintColor: Colors.grey,
+      ),
       home: const CurrencyConvertor(),
     );
   }
 }
 
 class CurrencyConvertor extends StatefulWidget {
-  const CurrencyConvertor({Key? key}) : super(key: key);
+  const CurrencyConvertor({super.key});
   @override
   State<CurrencyConvertor> createState() => _CurrencyConvertorState();
 }
@@ -29,7 +30,7 @@ class CurrencyConvertor extends StatefulWidget {
 class _CurrencyConvertorState extends State<CurrencyConvertor> {
   // Global Variables
   //TextEditingController inputValue = TextEditingController(); // global variable
-  double inputValue = 0.0;
+  double inputValue = 0;
   String? errorText;
   String? currency = 'RON';
   String? convertText = '';
@@ -37,75 +38,83 @@ class _CurrencyConvertorState extends State<CurrencyConvertor> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Currency Convertor',
-              style: TextStyle(color: Colors.yellowAccent)),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Currency Convertor',
+          style: TextStyle(color: Colors.yellowAccent),
         ),
-        body: SafeArea(
-          child: Container(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 0.0),
-            child: Column(
-              children: [
-                Image.network(
-                    'https://www.meme-arsenal.com/memes/8b5853d172e50f14ec7c53af8975c449.jpg'),
-                const Text(
-                  "PUT YOUR MONEY HERE",
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.red,
-                      fontWeight: FontWeight.w900),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: <Widget>[
+              Image.network(
+                'https://www.meme-arsenal.com/memes/8b5853d172e50f14ec7c53af8975c449.jpg',
+              ),
+              const Text(
+                'PUT YOUR MONEY HERE',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.red,
+                  fontWeight: FontWeight.w900,
                 ),
-                const SizedBox(height: 10),
-                TextField(
-                    //controller: inputValue,
-                    keyboardType:
-                        const TextInputType.numberWithOptions(decimal: true),
-                    decoration: InputDecoration(
-                      hintText: 'Enter the amount of EURO.',
-                      hintStyle: const TextStyle(fontSize: 20),
-                      errorText: errorText,
-                      errorStyle: const TextStyle(fontSize: 20),
-                      //labelText: 'Currency Convertor: EURO > RON',
-                      //labelStyle: const TextStyle(fontSize: 30),
-                    ),
-                    onChanged: (String input) {
-                      setState(() {
-                        final double? value = double.tryParse(input);
-                        if (value == null) {
-                          errorText = "Please enter a number.";
-                        } else {
-                          inputValue = (value * 4.5);
-                        }
-                      });
-                    }),
-                const SizedBox(height: 25),
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.currency_exchange_sharp, size: 30),
-                  label: const Text('Convert', style: TextStyle(fontSize: 30)),
-                  style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
-                      minimumSize: const Size(300, 60)),
-                  onPressed: () {
-                    setState(() {
-                      convertText = '$inputValue $currency';
-                    });
-                  },
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                //controller: inputValue,
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                decoration: InputDecoration(
+                  hintText: 'Enter the amount of EURO.',
+                  hintStyle: const TextStyle(fontSize: 20),
+                  errorText: errorText,
+                  errorStyle: const TextStyle(fontSize: 20),
+                  //labelText: 'Currency Convertor: EURO > RON',
+                  //labelStyle: const TextStyle(fontSize: 30),
                 ),
-                const SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 15.0, vertical: 0.0),
-                  child: Text(
-                    convertText!,
-                    style: const TextStyle(color: Colors.grey, fontSize: 30),
+                onChanged: (String input) {
+                  setState(() {
+                    final double? value = double.tryParse(input);
+                    if (value == null) {
+                      errorText = 'Please enter a number.';
+                    } else {
+                      inputValue = value * 4.5;
+                    }
+                  });
+                },
+              ),
+              const SizedBox(height: 25),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.currency_exchange_sharp, size: 30),
+                label: const Text('Convert', style: TextStyle(fontSize: 30)),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
                   ),
+                  minimumSize: const Size(300, 60),
                 ),
-              ],
-            ),
+                onPressed: () {
+                  setState(() {
+                    convertText = '$inputValue $currency';
+                  });
+                },
+              ),
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: Text(
+                  convertText!,
+                  style: const TextStyle(color: Colors.grey, fontSize: 30),
+                ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
